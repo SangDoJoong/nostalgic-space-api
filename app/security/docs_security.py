3684c21544e30782792872d3152d30062bce7115
chuneeeee <charie96@naver.com>
2024-03-31 20:24:44 +0900

    feat: docs 보안 설정 추가(2)

--- app/security/docs_security.py
+++ app/security/docs_security.py
@@ -1,28 +1,40 @@
+import base64
 import secrets
-from fastapi import FastAPI, HTTPException, status, Depends,APIRouter
-from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from fastapi.openapi.utils import  get_openapi
 
-security = HTTPBasic()
-router = APIRouter(
-    dependencies=[Depends(security)],
-)
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from typing import Any, Dict
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
 # 인증정보 
-name= "nostel_api"
-password="sandojoong"
+name= "sangdo"
+password = "sangdo"
 
+class ApidocBasicAuthMiddleware(BaseHTTPMiddleware):
 
-# User Verification Function
-def verification(creds: HTTPBasicCredentials = Depends(security)):
-
-    if creds.username == name and creds.password == password:
-        print("User Validated")
-        return True
-    else:
-        # From FastAPI 
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
+    async def dispatch(  # type: ignore
+            self, request: Request, call_next: RequestResponseEndpoint):
+        if request.url.path in ['/docs', '/openapi.json', '/redoc']:
+            auth_header = request.headers.get('Authorization')
+            if auth_header:
+                try:
+                    scheme, credentials = auth_header.split()
+                    if scheme.lower() == 'basic':
+                        decoded = base64.b64decode(credentials).decode('ascii')
+                        username, password = decoded.split(':')
+                        correct_username = secrets.compare_digest(
+                            username, name)
+                        correct_password = secrets.compare_digest(
+                            password, password)
+                        if correct_username and correct_password:
+                            return await call_next(request)
+                except Exception:
+                    ...
+            response = Response(content='Unauthorized', status_code=401)
+            response.headers['WWW-Authenticate'] = 'Basic'
+            return response
+        return await call_next(request)
