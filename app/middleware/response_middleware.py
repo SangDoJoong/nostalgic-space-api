24d54e153a4ad74fc982492ed544ae87c89ce1da
김동혁 <donghyeok.kim@medistream.co.kr>
2024-07-29 19:52:38 +0900

    fix: 모델 변경 / response_middleware 오류 수정

--- app/middleware/response_middleware.py
+++ app/middleware/response_middleware.py
@@ -1,35 +1,95 @@
-from fastapi import FastAPI, Request
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import Response, StreamingResponse
-import json
+# # from fastapi import FastAPI, Request
+# # from starlette.middleware.base import BaseHTTPMiddleware
+# # from starlette.responses import Response, StreamingResponse
+# # import json
+# # class ResponseMiddleware(BaseHTTPMiddleware):
+# #     async def dispatch(self, request: Request, call_next):
+# #         response = await call_next(request)
+# #         print(response)
+# #         # if isinstance(response, StreamingResponse):
+# #         #     print("!")
+# #         #     # For streaming responses, we need to capture the chunks, modify them, and re-stream
+# #         #     async def stream_response(response):
+# #         #         async for chunk in response.body_iterator:
+# #         #             # Modify chunk if necessary (for now, we are just passing it through)
+# #         #             yield chunk
 
-class ResponseMiddleware:
-    def __init__(self, app):
-        self.app = app
-
-    async def __call__(self, scope, receive, send):
-        await self.app(scope, receive, send)
-
-    async def dispatch(self, request, call_next):
-        response = await call_next(request)
+# #         #     return StreamingResponse(stream_response(response), status_code=response.status_code, headers=dict(response.headers))
         
-        if isinstance(response, StreamingResponse):
-            async def stream_response(response):
-                async for chunk in response.body_iterator:
-                    # 여기서 chunk를 원하는 방식으로 처리
-                    pass  
-            
-            await stream_response(response)
-            
-        else:
-            body = await response.body()
+# #         # else:
+# #         print("?")
+# #         body = await response.body()
+# #         print(body)
+# #         response_body = json.loads(body.decode())
+# #         modified_body = {"modified": response_body}
+# #         return Response(content=json.dumps(modified_body), status_code=response.status_code, headers=dict(response.headers), media_type="application/json")
 
-            # JSON 파싱 및 수정
+
+# import json
+# from starlette.middleware.base import BaseHTTPMiddleware
+# from starlette.requests import Request
+# from starlette.responses import Response, StreamingResponse
+
+# class ResponseMiddleware(BaseHTTPMiddleware):
+#     async def dispatch(self, request: Request, call_next):
+#         response = await call_next(request)
+
+#         if isinstance(response, StreamingResponse):
+#             print("?")
+#             # For streaming responses, we need to capture the chunks, modify them, and re-stream
+#             async def stream_response(response):
+#                 async for chunk in response.body_iterator:
+#                     # Modify chunk if necessary (for now, we are just passing it through)
+#                     yield chunk
+
+#             return StreamingResponse(stream_response(response), status_code=response.status_code, headers=dict(response.headers))
+        
+#         else:
+#             body = await response.body()
+#             response_body = json.loads(body.decode())
+#             modified_body = {"modified": response_body}
+#             print(modified_body)
+#             return Response(content=json.dumps(modified_body), status_code=response.status_code, headers=dict(response.headers), media_type="application/json")
+
+
+
+
+
+import json
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response, StreamingResponse
+from starlette.types import ASGIApp, Receive, Scope, Send
+
+class ResponseMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+
+        if isinstance(response, StreamingResponse):
+            # For streaming responses, we need to capture the chunks, modify them, and re-stream
+            original_chunks = []
+            async for chunk in response.body_iterator:
+                original_chunks.append(chunk)
+            
+            body = b''.join(original_chunks)
             response_body = json.loads(body.decode())
             modified_body = {"modified": response_body}
-
-            # 수정된 JSON을 다시 응답으로 설정
-            return Response(content=json.dumps(modified_body), status_code=response.status_code, headers=dict(response.headers), media_type="application/json")
+            modified_content = json.dumps(modified_body).encode()
+            
+            headers = dict(response.headers)
+            headers["Content-Length"] = str(len(modified_content))
+            
+            print(modified_content)
+            
+            return StreamingResponse(iter([modified_content]), status_code=response.status_code, headers=headers, media_type="application/json")
         
-        # 수정하지 않은 경우 그대로 반환
-        return response
+        else:
+            body = await response.body()
+            response_body = json.loads(body.decode())
+            modified_body = {"modified": response_body}
+            modified_content = json.dumps(modified_body).encode()
+            
+            headers = dict(response.headers)
+            headers["Content-Length"] = str(len(modified_content))
+            
+            return Response(content=modified_content, status_code=response.status_code, headers=headers, media_type="application/json")
