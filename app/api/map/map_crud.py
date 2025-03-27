5f941064edb28c764fe600b781feb3aee07ea143
김동혁 <donghyeok.kim@medistream.co.kr>
2025-03-27 17:29:25 +0900

    fix: map 모델 변경

--- app/api/map/map_crud.py
+++ app/api/map/map_crud.py
@@ -1,142 +1,38 @@
-from sqlalchemy import func
+# map_crud.py
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 
-from models import Map, Marker
+from models import Content, Map
+
+from .map_schema import MapCreate
 
 
-# 지도 CRUD 함수들
-def create_map(db: Session, map_in):
-    """
-    새로운 지도 레코드를 생성하여 데이터베이스에 저장한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param map_in: 'latitude'와 'longitude' 속성을 포함하는 입력 객체.
-    :return: 생성된 Map 인스턴스.
-    """
-    point = f"POINT({map_in.longitude} {map_in.latitude})"
-    db_map = Map(center_location=func.ST_GeomFromText(point, 4326))
+def create_map_marker(db: Session, map_data: MapCreate):
+    content = (
+        db.query(Content).filter(Content.contents_id == map_data.content_id).first()
+    )
+    if not content:
+        return None
+    db_map = Map(
+        content_id=map_data.content_id,
+        latitude=map_data.latitude,
+        longitude=map_data.longitude,
+        created_at=datetime.utcnow(),
+    )
     db.add(db_map)
-    db.commit()
+    try:
+        db.commit()
+    except Exception as e:
+        print(e)
     db.refresh(db_map)
+
     return db_map
 
 
-def get_map(db: Session, map_id: int):
-    """
-    지정된 ID를 가진 지도 레코드를 조회한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param map_id: 조회할 지도 ID.
-    :return: 조회된 Map 인스턴스 또는 존재하지 않을 경우 None.
-    """
+def get_map_marker(db: Session, map_id: int):
     return db.query(Map).filter(Map.map_id == map_id).first()
 
 
-def update_map(db: Session, map_id: int, map_in):
-    """
-    지정된 ID의 지도 레코드를 업데이트한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param map_id: 업데이트할 지도 ID.
-    :param map_in: 업데이트할 'latitude'와 'longitude' 값을 포함하는 입력 객체.
-    :return: 업데이트된 Map 인스턴스 또는 해당 레코드가 없을 경우 None.
-    """
-    db_map = get_map(db, map_id)
-    if not db_map:
-        return None
-    point = f"POINT({map_in.longitude} {map_in.latitude})"
-    db_map.center_location = func.ST_GeomFromText(point, 4326)
-    db.commit()
-    db.refresh(db_map)
-    return db_map
-
-
-def delete_map(db: Session, map_id: int):
-    """
-    지정된 ID의 지도 레코드를 삭제한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param map_id: 삭제할 지도 ID.
-    :return: 삭제된 Map 인스턴스 또는 해당 레코드가 없을 경우 None.
-    """
-    db_map = get_map(db, map_id)
-    if not db_map:
-        return None
-    db.delete(db_map)
-    db.commit()
-    return db_map
-
-
-# 마커 CRUD 함수들
-def create_marker(db: Session, marker_in):
-    """
-    새로운 마커 레코드를 생성하여 데이터베이스에 저장한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param marker_in: 'map_id', 'latitude', 'longitude', 'uid', 'content_id' 등의 속성을 포함하는 입력 객체.
-    :return: 생성된 Marker 인스턴스.
-    """
-    point = f"POINT({marker_in.longitude} {marker_in.latitude})"
-    db_marker = Marker(
-        map_id=marker_in.map_id,
-        latitude=marker_in.latitude,
-        longitude=marker_in.longitude,
-        location=func.ST_GeomFromText(point, 4326),
-        uid=marker_in.uid,
-        content_id=marker_in.content_id,
-    )
-    db.add(db_marker)
-    db.commit()
-    db.refresh(db_marker)
-    return db_marker
-
-
-def get_marker(db: Session, marker_id: int):
-    """
-    지정된 ID를 가진 마커 레코드를 조회한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param marker_id: 조회할 마커 ID.
-    :return: 조회된 Marker 인스턴스 또는 존재하지 않을 경우 None.
-    """
-    return db.query(Marker).filter(Marker.marker_id == marker_id).first()
-
-
-def update_marker(db: Session, marker_id: int, marker_in):
-    """
-    지정된 ID의 마커 레코드를 업데이트한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param marker_id: 업데이트할 마커 ID.
-    :param marker_in: 업데이트할 'map_id', 'latitude', 'longitude', 'uid', 'content_id' 등의 속성을 포함하는 입력 객체.
-    :return: 업데이트된 Marker 인스턴스 또는 해당 레코드가 없을 경우 None.
-    """
-    db_marker = get_marker(db, marker_id)
-    if not db_marker:
-        return None
-    point = f"POINT({marker_in.longitude} {marker_in.latitude})"
-    db_marker.map_id = marker_in.map_id
-    db_marker.latitude = marker_in.latitude
-    db_marker.longitude = marker_in.longitude
-    db_marker.location = func.ST_GeomFromText(point, 4326)
-    db_marker.uid = marker_in.uid
-    db_marker.content_id = marker_in.content_id
-    db.commit()
-    db.refresh(db_marker)
-    return db_marker
-
-
-def delete_marker(db: Session, marker_id: int):
-    """
-    지정된 ID의 마커 레코드를 삭제한다.
-
-    :param db: SQLAlchemy Session 인스턴스.
-    :param marker_id: 삭제할 마커 ID.
-    :return: 삭제된 Marker 인스턴스 또는 해당 레코드가 없을 경우 None.
-    """
-    db_marker = get_marker(db, marker_id)
-    if not db_marker:
-        return None
-    db.delete(db_marker)
-    db.commit()
-    return db_marker
+def get_marker_by_content(db: Session, content_id: int):
+    return db.query(Map).filter(Map.content_id == content_id).first()
--- app/api/map/map_router.py
+++ app/api/map/map_router.py
@@ -1,167 +1,204 @@
+# from fastapi import APIRouter, Depends, HTTPException
+# from sqlalchemy.orm import Session
+
+# from config.database_init import get_db
+
+# from . import map_crud, map_schema
+
+# router = APIRouter()
+
+# # 지도 관련 라우터
+
+
+# @router.post("/maps", response_model=map_schema.MapResponse)
+# def create_map(map_in: map_schema.MapCreate, db: Session = Depends(get_db)):
+#     """
+#     새 지도를 생성하는 엔드포인트.
+
+#     :param map_in: 생성할 지도 정보를 담은 MapCreate 스키마 객체. (예: 위도, 경도 등)
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 생성된 지도 정보를 담은 MapResponse 스키마 객체.
+#     """
+#     db_map = map_crud.create_map(db, map_in)
+#     return map_schema.MapResponse(
+#         map_id=db_map.map_id, latitude=map_in.latitude, longitude=map_in.longitude
+#     )
+
+
+# @router.get("/maps/{map_id}", response_model=map_schema.MapResponse)
+# def read_map(map_id: int, db: Session = Depends(get_db)):
+#     """
+#     지정한 ID의 지도를 조회하는 엔드포인트.
+
+#     :param map_id: 조회할 지도 ID.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 조회된 지도 정보를 담은 MapResponse 스키마 객체.
+#     :raises HTTPException: 지도 정보가 없을 경우 404 에러 발생.
+#     """
+#     db_map = map_crud.get_map(db, map_id)
+#     if not db_map:
+#         raise HTTPException(status_code=404, detail="Map not found")
+#     # center_location에서 위도/경도를 추출하는 처리는 상황에 맞게 구현 필요
+#     return map_schema.MapResponse(map_id=db_map.map_id, latitude=0.0, longitude=0.0)
+
+
+# @router.put("/maps/{map_id}", response_model=map_schema.MapResponse)
+# def update_map(
+#     map_id: int, map_in: map_schema.MapCreate, db: Session = Depends(get_db)
+# ):
+#     """
+#     지정한 ID의 지도 정보를 업데이트하는 엔드포인트.
+
+#     :param map_id: 업데이트할 지도 ID.
+#     :param map_in: 업데이트할 지도 정보를 담은 MapCreate 스키마 객체.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 업데이트된 지도 정보를 담은 MapResponse 스키마 객체.
+#     :raises HTTPException: 업데이트할 지도 정보가 없을 경우 404 에러 발생.
+#     """
+#     db_map = map_crud.update_map(db, map_id, map_in)
+#     if not db_map:
+#         raise HTTPException(status_code=404, detail="Map not found")
+#     return map_schema.MapResponse(
+#         map_id=db_map.map_id, latitude=map_in.latitude, longitude=map_in.longitude
+#     )
+
+
+# @router.delete("/maps/{map_id}")
+# def delete_map(map_id: int, db: Session = Depends(get_db)):
+#     """
+#     지정한 ID의 지도를 삭제하는 엔드포인트.
+
+#     :param map_id: 삭제할 지도 ID.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 삭제 결과를 나타내는 메시지.
+#     :raises HTTPException: 삭제할 지도 정보가 없을 경우 404 에러 발생.
+#     """
+#     db_map = map_crud.delete_map(db, map_id)
+#     if not db_map:
+#         raise HTTPException(status_code=404, detail="Map not found")
+#     return {"detail": "Map deleted"}
+
+
+# # 마커 관련 라우터
+
+
+# @router.post("/markers", response_model=map_schema.MarkerResponse)
+# def create_marker(marker_in: map_schema.MarkerCreate, db: Session = Depends(get_db)):
+#     """
+#     새 마커를 생성하는 엔드포인트.
+
+#     :param marker_in: 생성할 마커 정보를 담은 MarkerCreate 스키마 객체.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 생성된 마커 정보를 담은 MarkerResponse 스키마 객체.
+#     """
+#     db_marker = map_crud.create_marker(db, marker_in)
+#     return map_schema.MarkerResponse(
+#         marker_id=db_marker.marker_id,
+#         map_id=db_marker.map_id,
+#         latitude=marker_in.latitude,
+#         longitude=marker_in.longitude,
+#         uid=db_marker.uid,
+#         content_id=db_marker.content_id,
+#     )
+
+
+# @router.get("/markers/{marker_id}", response_model=map_schema.MarkerResponse)
+# def read_marker(marker_id: int, db: Session = Depends(get_db)):
+#     """
+#     지정한 ID의 마커를 조회하는 엔드포인트.
+
+#     :param marker_id: 조회할 마커 ID.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 조회된 마커 정보를 담은 MarkerResponse 스키마 객체.
+#     :raises HTTPException: 조회할 마커 정보가 없을 경우 404 에러 발생.
+#     """
+#     db_marker = map_crud.get_marker(db, marker_id)
+#     if not db_marker:
+#         raise HTTPException(status_code=404, detail="Marker not found")
+#     return map_schema.MarkerResponse(
+#         marker_id=db_marker.marker_id,
+#         map_id=db_marker.map_id,
+#         latitude=db_marker.latitude,
+#         longitude=db_marker.longitude,
+#         uid=db_marker.uid,
+#         content_id=db_marker.content_id,
+#     )
+
+
+# @router.put("/markers/{marker_id}", response_model=map_schema.MarkerResponse)
+# def update_marker(
+#     marker_id: int, marker_in: map_schema.MarkerCreate, db: Session = Depends(get_db)
+# ):
+#     """
+#     지정한 ID의 마커 정보를 업데이트하는 엔드포인트.
+
+#     :param marker_id: 업데이트할 마커 ID.
+#     :param marker_in: 업데이트할 마커 정보를 담은 MarkerCreate 스키마 객체.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 업데이트된 마커 정보를 담은 MarkerResponse 스키마 객체.
+#     :raises HTTPException: 업데이트할 마커 정보가 없을 경우 404 에러 발생.
+#     """
+#     db_marker = map_crud.update_marker(db, marker_id, marker_in)
+#     if not db_marker:
+#         raise HTTPException(status_code=404, detail="Marker not found")
+#     return map_schema.MarkerResponse(
+#         marker_id=db_marker.marker_id,
+#         map_id=db_marker.map_id,
+#         latitude=marker_in.latitude,
+#         longitude=marker_in.longitude,
+#         uid=db_marker.uid,
+#         content_id=db_marker.content_id,
+#     )
+
+
+# @router.delete("/markers/{marker_id}")
+# def delete_marker(marker_id: int, db: Session = Depends(get_db)):
+#     """
+#     지정한 ID의 마커를 삭제하는 엔드포인트.
+
+#     :param marker_id: 삭제할 마커 ID.
+#     :param db: 데이터베이스 세션 (SQLAlchemy Session).
+#     :return: 삭제 결과를 나타내는 메시지.
+#     :raises HTTPException: 삭제할 마커 정보가 없을 경우 404 에러 발생.
+#     """
+#     db_marker = map_crud.delete_marker(db, marker_id)
+#     if not db_marker:
+#         raise HTTPException(status_code=404, detail="Marker not found")
+#     return {"detail": "Marker deleted"}
+
+# router.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+import api.map.map_crud as map_crud
 from config.database_init import get_db
 
-from . import map_crud, map_schema
+from .map_schema import MapCreate, MapResponse
 
-router = APIRouter()
-
-# 지도 관련 라우터
-
-
-@router.post("/maps", response_model=map_schema.MapResponse)
-def create_map(map_in: map_schema.MapCreate, db: Session = Depends(get_db)):
-    """
-    새 지도를 생성하는 엔드포인트.
-
-    :param map_in: 생성할 지도 정보를 담은 MapCreate 스키마 객체. (예: 위도, 경도 등)
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 생성된 지도 정보를 담은 MapResponse 스키마 객체.
-    """
-    db_map = map_crud.create_map(db, map_in)
-    return map_schema.MapResponse(
-        map_id=db_map.map_id, latitude=map_in.latitude, longitude=map_in.longitude
-    )
+router = APIRouter(
+    prefix="/api/map",
+)
 
 
-@router.get("/maps/{map_id}", response_model=map_schema.MapResponse)
-def read_map(map_id: int, db: Session = Depends(get_db)):
-    """
-    지정한 ID의 지도를 조회하는 엔드포인트.
-
-    :param map_id: 조회할 지도 ID.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 조회된 지도 정보를 담은 MapResponse 스키마 객체.
-    :raises HTTPException: 지도 정보가 없을 경우 404 에러 발생.
-    """
-    db_map = map_crud.get_map(db, map_id)
+@router.post("/create", response_model=MapResponse)
+def map_marker_create(map_data: MapCreate, db: Session = Depends(get_db)):
+    db_map = map_crud.create_map_marker(db, map_data)
     if not db_map:
-        raise HTTPException(status_code=404, detail="Map not found")
-    # center_location에서 위도/경도를 추출하는 처리는 상황에 맞게 구현 필요
-    return map_schema.MapResponse(map_id=db_map.map_id, latitude=0.0, longitude=0.0)
+        raise HTTPException(status_code=404, detail="Content not found")
+    return db_map
 
 
-@router.put("/maps/{map_id}", response_model=map_schema.MapResponse)
-def update_map(
-    map_id: int, map_in: map_schema.MapCreate, db: Session = Depends(get_db)
-):
-    """
-    지정한 ID의 지도 정보를 업데이트하는 엔드포인트.
-
-    :param map_id: 업데이트할 지도 ID.
-    :param map_in: 업데이트할 지도 정보를 담은 MapCreate 스키마 객체.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 업데이트된 지도 정보를 담은 MapResponse 스키마 객체.
-    :raises HTTPException: 업데이트할 지도 정보가 없을 경우 404 에러 발생.
-    """
-    db_map = map_crud.update_map(db, map_id, map_in)
-    if not db_map:
-        raise HTTPException(status_code=404, detail="Map not found")
-    return map_schema.MapResponse(
-        map_id=db_map.map_id, latitude=map_in.latitude, longitude=map_in.longitude
-    )
+@router.get("/{map_id}", response_model=MapResponse)
+def map_marker_get(map_id: int, db: Session = Depends(get_db)):
+    map_marker = map_crud.get_map_marker(db, map_id)
+    if not map_marker:
+        raise HTTPException(status_code=404, detail="Map marker not found")
+    return map_marker
 
 
-@router.delete("/maps/{map_id}")
-def delete_map(map_id: int, db: Session = Depends(get_db)):
-    """
-    지정한 ID의 지도를 삭제하는 엔드포인트.
-
-    :param map_id: 삭제할 지도 ID.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 삭제 결과를 나타내는 메시지.
-    :raises HTTPException: 삭제할 지도 정보가 없을 경우 404 에러 발생.
-    """
-    db_map = map_crud.delete_map(db, map_id)
-    if not db_map:
-        raise HTTPException(status_code=404, detail="Map not found")
-    return {"detail": "Map deleted"}
-
-
-# 마커 관련 라우터
-
-
-@router.post("/markers", response_model=map_schema.MarkerResponse)
-def create_marker(marker_in: map_schema.MarkerCreate, db: Session = Depends(get_db)):
-    """
-    새 마커를 생성하는 엔드포인트.
-
-    :param marker_in: 생성할 마커 정보를 담은 MarkerCreate 스키마 객체.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 생성된 마커 정보를 담은 MarkerResponse 스키마 객체.
-    """
-    db_marker = map_crud.create_marker(db, marker_in)
-    return map_schema.MarkerResponse(
-        marker_id=db_marker.marker_id,
-        map_id=db_marker.map_id,
-        latitude=marker_in.latitude,
-        longitude=marker_in.longitude,
-        uid=db_marker.uid,
-        content_id=db_marker.content_id,
-    )
-
-
-@router.get("/markers/{marker_id}", response_model=map_schema.MarkerResponse)
-def read_marker(marker_id: int, db: Session = Depends(get_db)):
-    """
-    지정한 ID의 마커를 조회하는 엔드포인트.
-
-    :param marker_id: 조회할 마커 ID.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 조회된 마커 정보를 담은 MarkerResponse 스키마 객체.
-    :raises HTTPException: 조회할 마커 정보가 없을 경우 404 에러 발생.
-    """
-    db_marker = map_crud.get_marker(db, marker_id)
-    if not db_marker:
-        raise HTTPException(status_code=404, detail="Marker not found")
-    return map_schema.MarkerResponse(
-        marker_id=db_marker.marker_id,
-        map_id=db_marker.map_id,
-        latitude=db_marker.latitude,
-        longitude=db_marker.longitude,
-        uid=db_marker.uid,
-        content_id=db_marker.content_id,
-    )
-
-
-@router.put("/markers/{marker_id}", response_model=map_schema.MarkerResponse)
-def update_marker(
-    marker_id: int, marker_in: map_schema.MarkerCreate, db: Session = Depends(get_db)
-):
-    """
-    지정한 ID의 마커 정보를 업데이트하는 엔드포인트.
-
-    :param marker_id: 업데이트할 마커 ID.
-    :param marker_in: 업데이트할 마커 정보를 담은 MarkerCreate 스키마 객체.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 업데이트된 마커 정보를 담은 MarkerResponse 스키마 객체.
-    :raises HTTPException: 업데이트할 마커 정보가 없을 경우 404 에러 발생.
-    """
-    db_marker = map_crud.update_marker(db, marker_id, marker_in)
-    if not db_marker:
-        raise HTTPException(status_code=404, detail="Marker not found")
-    return map_schema.MarkerResponse(
-        marker_id=db_marker.marker_id,
-        map_id=db_marker.map_id,
-        latitude=marker_in.latitude,
-        longitude=marker_in.longitude,
-        uid=db_marker.uid,
-        content_id=db_marker.content_id,
-    )
-
-
-@router.delete("/markers/{marker_id}")
-def delete_marker(marker_id: int, db: Session = Depends(get_db)):
-    """
-    지정한 ID의 마커를 삭제하는 엔드포인트.
-
-    :param marker_id: 삭제할 마커 ID.
-    :param db: 데이터베이스 세션 (SQLAlchemy Session).
-    :return: 삭제 결과를 나타내는 메시지.
-    :raises HTTPException: 삭제할 마커 정보가 없을 경우 404 에러 발생.
-    """
-    db_marker = map_crud.delete_marker(db, marker_id)
-    if not db_marker:
-        raise HTTPException(status_code=404, detail="Marker not found")
-    return {"detail": "Marker deleted"}
+@router.get("/content/{content_id}", response_model=MapResponse)
+def marker_by_content_get(content_id: int, db: Session = Depends(get_db)):
+    map_marker = map_crud.get_marker_by_content(db, content_id)
+    if not map_marker:
+        raise HTTPException(status_code=404, detail="Marker for content not found")
+    return map_marker
