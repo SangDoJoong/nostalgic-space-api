d3bb1a5c0d47062ed249461b0ca6d69d244ebeec
YuSunjo <tnswh2023@gmail.com>
2025-05-09 18:08:49 +0900

    feat: fastapi, pydantic 등 라이브러리 버전업

--- app/api/user/schema/user_create_schema.py
+++ app/api/user/schema/user_create_schema.py
@@ -1,23 +1,10 @@
-"""
-사용자 관련 스키마 정의 모듈.
-
-이 모듈은 사용자 생성 및 인증 토큰에 사용되는 Pydantic 모델을 정의합니다.
-
-작성자:
-    kimdonghyeok
-"""
-
 from fastapi import HTTPException
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
+from pydantic_core.core_schema import ValidationInfo
 from starlette import status
 
 
-class BaseOrmModel(BaseModel):
-    class Config:
-        orm_mode = True
-
-
-class UserCreate(BaseOrmModel):
+class UserCreate(BaseModel):
     """
     사용자 생성 요청 데이터 모델.
 
@@ -35,7 +22,8 @@ class UserCreate(BaseOrmModel):
     password1: str
     password2: str
 
-    @validator("username", "password1", "password2")
+    @field_validator("username", "password1", "password2")
+    @classmethod
     def not_empty(cls, v):
         """
         필드 값이 비어 있지 않은지 검증합니다.
@@ -56,8 +44,9 @@ class UserCreate(BaseOrmModel):
             )
         return v
 
-    @validator("password2")
-    def passwords_match(cls, v, values):
+    @field_validator("password2")
+    @classmethod
+    def passwords_match(cls, v, info: ValidationInfo):
         """
         `password1`과 `password2`가 일치하는지 확인합니다.
 
@@ -71,24 +60,9 @@ class UserCreate(BaseOrmModel):
         Raises:
             HTTPException: 두 비밀번호가 일치하지 않을 경우 400 상태 코드 반환.
         """
-        if "password1" in values and v != values["password1"]:
+        if "password1" in info.data and v != info.data["password1"]:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="비밀번호가 일치하지 않습니다",
             )
         return v
-
-
-class Token(BaseOrmModel):
-    """
-    인증 토큰 데이터 모델.
-
-    Attributes:
-        access_token (str): 인증에 사용되는 액세스 토큰.
-        token_type (str): 토큰 유형 (예: Bearer).
-        username (str): 토큰이 발급된 사용자의 이름.
-    """
-
-    access_token: str
-    token_type: str
-    username: str
